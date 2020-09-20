370373ff0140e2774b02c382d9fbb91f69ea4751
Clarissa Labrum <clarissa.labrum@gmail.com>
2020-09-19 20:43:49 -0600

    task 1

---
+++ homework/homework2/JustABunny/src/JustABunny.java
@@ -0,0 +1,7 @@
+public class JustABunny {
+    public static void main(String[] args) {
+        System.out.println("It's only a bunny");
+        System.out.println("Well, it's always the same, I always tell them, " +
+                "do they listen to me?");
+    }
+}
