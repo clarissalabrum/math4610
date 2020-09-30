f45c6351912f25c5685b696787d83abaeaef5448
Clarissa Labrum <clarissa.labrum@gmail.com>
2020-09-30 13:46:42 -0600

    update task 5 code

--- homework/homework2/Task5/src/Task5.java
+++ homework/homework2/Task5/src/Task5.java
@@ -11,23 +11,27 @@ public class Task5 {
             h.add(h.get(2+i)/10);
         }
 
+        //get the approximate value for each h
         ArrayList<Double> approx = new ArrayList<>();
         for (int i = 0; i < h.size(); i++) {
             approx.add((Math.cos(2+h.get(i)) - 2*Math.cos(2) +
                     Math.cos(2-h.get(i)))/ (h.get(i)*h.get(i)));
         }
 
+        //calculate exact value
         double exact = -1*Math.cos(2);
 
+        //calculate the difference
         ArrayList<Double> diff = new ArrayList<>();
         for (int i = 0; i < h.size(); i++) {
             diff.add(exact - approx.get(i));
         }
 
+        //print results
         System.out.println("exact: " + exact);
-        System.out.printf("%-10s%-10s%-10s\n", "h", "approx", "diff");
+        System.out.printf("%-5s%-10s%10s%11s\n", "iter", "   h", "approx", "diff");
         for (int i = 0; i < h.size(); i++) {
-            System.out.printf("%-10.2e%-10.2e%-10.2e\n", h.get(i), approx.get(i),
+            System.out.printf("%-5d%-10.2e%10.2e%11.2e\n", i, h.get(i), approx.get(i),
                     diff.get(i));
         }
     }
