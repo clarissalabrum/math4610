719d5a12089475d136e9bd030ccd6ca69c52a0df
Clarissa Labrum <clarissa.labrum@gmail.com>
2020-10-21 20:58:24 -0600

    task 5

--- homework/homework3/Task3/precsion/src/SinglePrecision.java
+++ homework/homework3/Task3/precsion/src/SinglePrecision.java
@@ -4,6 +4,6 @@ public class SinglePrecision {
         while (1 < 1 + num){
             num = num * 0.5f;
         }
-        System.out.println(num * 2);
+        System.out.println(num * 2.0f);
     }
 }
